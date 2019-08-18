eae365516b48f3f1b2956d3c7f735a084079138a
pkac750 <kaczmarczyk.pa@gmail.com>
2019-08-18 16:52:37 +0100

    The base starting point for the Blueberry Core

---
+++ Source/BlueBerry.Core/IClientSetupApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// The core functionality shared between all parts of the Blueberry application
+/// </summary>
+
+namespace BlueBerry.Core
+{
+    /// <summary>
+    /// The entry point int the Bluberry application used to sestup the environment.
+    /// All top level application should create an instance of this.
+    /// </summary>
+
+    public interface IClientSetupApplication
+    {
+
+    }
+}
